60e328d5c2c9ca4d575a95f9db519e31ad849856
Christoph Meyer <christoph@chmey.com>
2022-06-03 19:00:46 +0200

    Add SmtpTo option to config and add named tests

--- cfg/cfg_test.go
+++ cfg/cfg_test.go
@@ -12,14 +12,14 @@ func TestReadConfig(t *testing.T) {
 	assert := assert.New(t)
 	require := require.New(t)
 
-	var tests = []struct {
+	var tests = map[string]struct {
 		configPath  string
 		wantErr     bool
 		expectedCfg *CanaryConfig
 		wantWrite   bool
 		writeCfg    string
 	}{
-		{
+		"valid config": {
 			configPath: "config.test.toml",
 			wantErr:    false,
 			expectedCfg: &CanaryConfig{
@@ -34,6 +34,7 @@ Last Line Of Document
 				SmtpPass:    "INSECURE_PASSWORD",
 				SmtpFrom:    "example@example.com",
 				SmtpSubject: "Email Subject",
+				SmtpTo:      []string{"admin@example.com"},
 			},
 			wantWrite: true,
 			writeCfg: `
@@ -49,16 +50,17 @@ smtpUser = "example@example.com"
 smtpPass = "INSECURE_PASSWORD"
 smtpFrom = "example@example.com"
 smtpSubject = "Email Subject"
+smtpTo = [ "admin@example.com" ]
 `,
 		},
-		{
+		"config does not exist": {
 			configPath:  "DOESNOTEXIST",
 			wantErr:     true,
 			expectedCfg: nil,
 			wantWrite:   false,
 			writeCfg:    "",
 		},
-		{
+		"invalid config": {
 			configPath:  "config.test.toml",
 			wantErr:     true,
 			expectedCfg: nil,
@@ -67,25 +69,68 @@ smtpSubject = "Email Subject"
 canaryFileName ={= "test_canary.txt"
 `,
 		},
+		"array type is string": {
+			configPath:  "config.test.toml",
+			wantErr:     true,
+			expectedCfg: nil,
+			wantWrite:   true,
+			writeCfg: `
+canaryFileName = "test_canary.txt"
+canaryDocument = """
+First Line Of Document
+Last Line Of Document
+"""
+smtpHost = "example.com"
+smtpPort = 465
+smtpProto = "SSL"
+smtpUser = "example@example.com"
+smtpPass = "INSECURE_PASSWORD"
+smtpFrom = "example@example.com"
+smtpSubject = "Email Subject"
+smtpTo = "admin@example.com"
+`,
+		},
+		"int type is string": {
+			configPath:  "config.test.toml",
+			wantErr:     true,
+			expectedCfg: nil,
+			wantWrite:   true,
+			writeCfg: `
+canaryFileName = "test_canary.txt"
+canaryDocument = """
+First Line Of Document
+Last Line Of Document
+"""
+smtpHost = "example.com"
+smtpPort = "465"
+smtpProto = "SSL"
+smtpUser = "example@example.com"
+smtpPass = "INSECURE_PASSWORD"
+smtpFrom = "example@example.com"
+smtpSubject = "Email Subject"
+smtpTo = [ "admin@example.com" ]
+`,
+		},
 	}
-	for _, tc := range tests {
-		if tc.wantWrite {
-			err := os.WriteFile(tc.configPath, []byte(tc.writeCfg), 0644)
-			require.NoError(err)
-		}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			if tc.wantWrite {
+				err := os.WriteFile(tc.configPath, []byte(tc.writeCfg), 0644)
+				require.NoError(err)
+			}
 
-		cfg, err := NewConfig(tc.configPath)
-		if !tc.wantErr {
-			require.NoError(err)
-			assert.Equal(*cfg, *tc.expectedCfg)
-		} else {
-			require.Error(err)
-		}
+			cfg, err := NewConfig(tc.configPath)
+			if !tc.wantErr {
+				require.NoError(err)
+				assert.Equal(*cfg, *tc.expectedCfg)
+			} else {
+				require.Error(err)
+			}
 
-		if tc.wantWrite {
-			err := os.Remove(tc.configPath)
-			require.NoError(err)
-		}
+			if tc.wantWrite {
+				err := os.Remove(tc.configPath)
+				require.NoError(err)
+			}
+		})
 	}
-
 }
